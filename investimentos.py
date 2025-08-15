d0168324ad6f6fc83cb61ddff2a5efae225baccf
cantaruttim <cantaruttim@outlook.com>
2025-08-14 21:32:26 -0300

    tratamentos iniciais

--- investimentos.py
+++ investimentos.py
@@ -23,12 +23,29 @@ def ler_taxa_selic(path_file):
     )
     return selic    
 
+## Dados de investimentos
 df = ler_investimentos(path_file, sheet)
 df["acumulado_investido"] = df["Valor"].sum()
 print(df.head(10))
 
 print()
 
+## Dados da Selic - Bacen
 selic = ler_taxa_selic(taxa_selic)
-print(selic.head(10))
 
+def tratamentos_iniciais(df, columns):
+
+    # Remove espaços extras nos nomes das colunas
+    df.columns = df.columns.str.strip()
+
+    df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y', errors='coerce')
+    
+
+    cols = ["Taxa_aa", "Taxa_media", "Taxa_mediana", "Taxa_modal"]
+    for col in cols:
+        df[col] = df[col].str.replace(",", ".", regex=False).astype(float)
+
+    return df
+
+selic = tratamentos_iniciais(selic)
+print(selic)
